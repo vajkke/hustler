c90c706a556e126fa233d4eb59ddab86a38dd286
Aleksandar V <95486215+vajkke@users.noreply.github.com>
2022-11-25 01:31:40 +0100

    finished sidebar display

--- js/game/sidebar/sidebarDisplay/shop.js
+++ js/game/sidebar/sidebarDisplay/shop.js
@@ -123,6 +123,95 @@ const shopShopHTML = `
 </div>
 `;
 
+const exchangeHTML = `
+<div class="converter--buttons">
+<div class="dollars-to-gold--container">
+  <div class="cost-container">
+    <h1 class="exchange-cost--heading">Cost</h1>
+    <div class="cost-display--container">
+      <h1 class="cost-money-text">
+        $ <span class="cost-money-value">1000000</span>
+      </h1>
+    </div>
+  </div>
+  <div class="result-container">
+    <h1 class="exchange-result--heading">Result</h1>
+    <div class="result-display--container">
+      <h1 class="result-gold-text">
+        <img src="./img/shop/gold-exchange.svg" />
+        <span class="result-gold-value">1</span>
+      </h1>
+    </div>
+  </div>
+</div>
+
+<div class="buttons-container">
+  <div class="arrow-up--container arrow">
+    <span class="material-symbols-outlined arrow-up">
+      keyboard_arrow_up
+    </span>
+  </div>
+  <div class="arrow-down--container arrow">
+    <span class="material-symbols-outlined arrow-down">
+      keyboard_arrow_down
+    </span>
+  </div>
+</div>
+</div>
+<div class="exchange-button">EXCHANGE</div>
+`;
+
+const freeGoldHTML = `
+<div class="free-left-side">
+<div class="social-media--container">
+  <div class="social-media github">
+    <img
+      src="./img/shop/github.svg"
+      class="social-media--img github-img"
+    />
+    <h1 class="social-media-text">Star this repository</h1>
+    <a href="#"><div class="social-media--btn">Go</div></a>
+  </div>
+  <div class="social-media linkedin">
+    <img
+      src="./img/shop/linkedin.svg"
+      class="social-media--img linkedin-img"
+    />
+    <h1 class="social-media-text">Add on Linkedin</h1>
+    <a href="#"><div class="social-media--btn">Go</div></a>
+  </div>
+
+  <div class="social-media youtube">
+    <img
+      src="./img/shop/youtube.svg"
+      class="social-media--img youtube-img"
+    />
+    <h1 class="social-media-text">Subscribe on Youtube</h1>
+    <a href="#"><div class="social-media--btn">Go</div></a>
+  </div>
+  <div class="social-media twitter">
+    <img
+      src="./img/shop/twitter.svg"
+      class="social-media--img twitter-img"
+    />
+    <h1 class="social-media-text">Follow on Twitter</h1>
+    <a href="#"><div class="social-media--btn">Go</div></a>
+  </div>
+</div>
+</div>
+<div class="free-right-side">
+<div class="promo-code--container">
+  <h1 class="promo-code--heading">
+    Use promo code and get free gold
+  </h1>
+  <form name="promo-code" class="promo-code--form">
+    <input type="text" class="promo-code--input" />
+    <input type="button" value="apply" class="promo-codeBtn" />
+  </form>
+</div>
+</div>
+`;
+
 const shopOpenFunction = () => {
   // removing others
   upgradesContainer.innerHTML = "";
@@ -138,6 +227,8 @@ const shopOpenFunction = () => {
 
   // refreshing & displaying shop
   shopShopContainer.innerHTML = "";
+  shopExchangeContainer.innerHTML = "";
+  shopFreeContainer.innerHTML = "";
 
   shopContainer.innerHTML = "";
   shopContainer.innerHTML += shopHTML;
@@ -146,6 +237,10 @@ const shopOpenFunction = () => {
   // deciding which shop to display -- nepotrebno realno
   if (shopWrapper.getAttribute("option") === "shop") {
     shopShopContainer.innerHTML += shopShopHTML;
+  } else if (shopWrapper.getAttribute("option") === "exchange") {
+    shopExchangeContainer.innerHTML += exchangeHTML;
+  } else if (shopWrapper.getAttribute("option") === "free") {
+    shopFreeContainer.innerHTML += freeGoldHTML;
   }
 };
 

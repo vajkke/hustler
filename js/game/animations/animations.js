4e7e7b393110f21036ead9c726d3974f41946b0a
Aleksandar V <95486215+vajkke@users.noreply.github.com>
2023-01-15 19:55:45 +0100

    added mobile version

--- js/game/animations/animations.js
+++ js/game/animations/animations.js
@@ -1,7 +1,11 @@
 import unlockingJobs from "./unlockingJob.js";
+import upgradersBuy from "./upgradesBuy.js";
+import managersBuy from "./managersBuy.js";
 
 const animations = () => {
   unlockingJobs();
+  upgradersBuy();
+  managersBuy();
 };
 
 export default animations;
--- js/game/sidebar/sidebar.js
+++ js/game/sidebar/sidebar.js
@@ -1,7 +1,9 @@
 import sidebarDisplayFunction from "./sidebarDisplay/sidebarDisplay.js";
+import sidebarMobile from "./sidebarMobile/sidebarMobile.js";
 
 const sidebar = () => {
   sidebarDisplayFunction();
+  sidebarMobile();
 };
 
 export default sidebar;
